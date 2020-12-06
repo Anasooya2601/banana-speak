810cfd09bbf5773052d3334a3b123edd67d4d9b7
Anasooya2601 <anasooyadutta@gmail.com>
2020-12-06 18:32:30 +0530

    final commit

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
-console.log(txtInput)
-function clickHandler() {
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickHandler)
+
+btnTranslate.addEventListener("click", clickHandler)
 
 
 
